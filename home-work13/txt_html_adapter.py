1999169c039df5cded1d73ae5a8a32e3ee227958
Serhii-Study <adyvan2008@gmail.com>
2023-07-03 18:58:14 +0300

    Home work 13, lesson 15

---
+++ home-work13/txt_html_adapter.py
@@ -0,0 +1,20 @@
+
+from html_render import HTMLRenderer
+from txt_taken import TxtData
+
+
+class TxtToHtmlAdapter:
+    def __init__(self, file_path):
+        self.txt_data = TxtData(file_path)
+
+    def convert_to_html(self):
+        html_renderer = HTMLRenderer()
+        fields = self.txt_data.get_fields()
+        records = self.txt_data.get_records()
+
+        html_output = ""
+        for record in records:
+            rendered_record = html_renderer.render_record(record)
+            html_output += rendered_record
+
+        return html_output
