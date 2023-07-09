45c606baeaa4aec69cb26f44842bd75fdda54c32
Serhii-Study <adyvan2008@gmail.com>
2023-07-09 22:07:56 +0300

    Home work 15

---
+++ Home-work15/locators.py
@@ -0,0 +1,38 @@
+# site https://varus.ua/
+
+# xPath and CSS locators
+
+# 1 xpath //a/button, CSS .sf-link .sf-button,sf-banner-pre-header__button
+# 2 xpath //p[@class="company-phone o-header__phone-contact"]/a, CSS .sf-link,company-phone__link,company-phone__link--sm
+# 3 xpath //div[@class="o-header__col o-header__col--left"]/a/div[@class="sf-image sf-header__logo"]/img[@alt="VARUS"],
+# CSS .o-header-wrapper,pre-header-exist .sf-image,sf-header__logo img[stylesrc= "/assets/logo.svg"]
+# 4 xpath //img[@class="m-home-apps__svg"]/../../../../../../div[@class="m-home-banner__col m-home-banner__apps"],
+# CSS .m-home-banner__apps .m-home-apps
+# 5 xpath //a[@class="sf-link m-social-contact__link m-social-contact__link-inst"], CSS .m-social-contact .m-social-contact__link-inst
+# 6 xpath //a[@class="sf-link m-social-contact__link m-social-contact__link-twitter"], CSS .m-social-contact .m-social-contact__link-fb
+# 7 xpath //a[@class="sf-link m-social-contact__link m-social-contact__link-fb"], CSS .m-social-contact .m-social-contact__link-twitter
+# 8 xpath //button[@class="sf-button m-seo-block__link sf-link sf-link--primary"], CSS .sf-button.m-seo-block__link,sf-link sf-link--primary
+# 9 xpath //a[@class="sf-link sf-link--primary all-products-link"]/../../../../div[@id="esputnik-widget-product-offers-main"],
+# CSS #esputnik-widget-product-offers-main
+# 10 xpath //a[@class="sf-link sf-link--primary all-products-link"]/../../div[@class="o-section__mobile-banner only-banner"],
+# CSS .router-link-active .sf-image,sf-header__logo .img[alt="Varus"]
+# 11 xpath //a[@class="sf-link sf-link--primary"]
+# 12 xpath //a/button[@class="sf-button sf-banner-pre-header__button"]
+# 13 xpath //button[@class="sf-button sf-link sf-link--primary a-toggle-menu o-header__toggle-menu"]
+# 14 xpath //div[@class="sf-image m-brands-carousel__image"]/img[@alt="Borjomi"]
+# 15 xpath //div[@class="sf-image m-brands-carousel__image"]/img[@alt="Kinder"]
+# 16 xpath //div[@class="sf-image m-brands-carousel__image"]/img[@alt="Martini"]
+# 17 xpath //div[@class="sf-image m-brands-carousel__image"]/img[@alt="Оболонь"]
+# 18 xpath //div[@class="sf-image m-brands-carousel__image"]/img[@alt="Coca-Cola"]
+# 19 xpath //p[@class="company-phone"]/a
+# 20 xpath //div[@class="sf-footer-column__content"]/ul/li/a[@href="/promotion"]
+# 21 xpath //div[@class="sf-footer-column__content"]/ul/li/a[@href="/gazeta-varus-0"]
+# 22 xpath //div[@class="sf-footer-column__content"]/ul/li/a[@href="/novelty"]
+# 23 xpath //div[@class="sf-footer-column__content"]/ul/li/a[@href="/stores"]
+# 24 xpath //div[@class="sf-footer-column__content"]/ul/li/a[@href="/loyalty"]
+# 25 xpath //div[@class="sf-footer-column__content"]/ul/li/a[@href="/own-tm"]
+# 26 xpath //div[@class="sf-footer-column__content"]/ul/li/a[@href="/about-company"]
+# 27 xpath //div[@class="sf-bottom-navigation-item sf-bottom-navigation-item__catalog sf-bottom-navigation-item--center"]
+# 28 xpath //div[@class="sf-bottom-navigation-item sf-bottom-navigation-item__search sf-bottom-navigation-item--center"]
+# 29 xpath //div[@class="sf-bottom-navigation-item sf-bottom-navigation-item__profile sf-bottom-navigation-item--center"]
+# 30 xpath //div[@class="sf-bottom-navigation-item o-bottom-navigation__cart sf-bottom-navigation-item__cart sf-bottom-navigation-item--center"]
