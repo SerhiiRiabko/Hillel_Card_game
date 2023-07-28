ba4fee0e15ed61bd3ed7655ef8c6dddab1dfcc46
Serhii-Study <adyvan2008@gmail.com>
2023-07-28 12:25:39 +0300

    Changes according comments

--- home-work13/txt_proxt_writter.py
+++ home-work13/txt_proxt_writter.py
@@ -1,12 +1,20 @@
 class TxtWriterProxy:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.data = []
 
-    def write_data(self, record):
-        self.data.append(record)
+    def __init__(self, filename):
+        self.file_path = filename
+        self.data = []
+        self.modified = False
+
+    def add_log(self, log_message):
+        self.data.append(log_message)
+        self.modified = True
 
     def save_data(self):
+        if not self.modified:
+            return  # No need to write if the data has not been modified
+
         with open(self.file_path, 'w') as file:
             for record in self.data:
                 file.write(record + '\n')
+
+        self.modified = False
